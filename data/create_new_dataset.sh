b878ea83c0ae71c84d68a39c19dd02d559265a3e
Michael Moffatt <mmoffatt@umich.edu>
2024-12-10 20:57:30 -0500

    updated create dataset script

--- data/create_new_dataset.sh
+++ data/create_new_dataset.sh
@@ -7,6 +7,8 @@ pushd "$new_dataset"
 # Use softlinks so we can use template/prepare.py for development
 ln -s ../template/prepare.py prepare.py
 ln -s ../template/utils ./utils
+ln -s ../template/tests.py tests.py
+ln -s ../template/tokenizer_options.py tokenizer_options.py
 
 # Different datasets may have different phoneme sets
 cp ../template/get_dataset.sh get_dataset.sh
